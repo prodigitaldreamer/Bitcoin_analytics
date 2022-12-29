5f7702381c3185cbd846c2144fca337b59ddc21b
prodigitaldreamer <smartec414+1@gmail.com>
2022-12-29 17:35:20 +0100

    empty main
    currently we want an empty main routine

--- src/main.py
+++ src/main.py
@@ -3,20 +3,12 @@
 # @author       criticalEntropy
 # @date         23.12.2022
 #############################################################
-from btc_parser import BtcAddressMonitoring
 
 
 def main():
 
     print("Hello World!")
-
-    test_address = "3MCMZjWAMdGqAhi1iF1oyiiE2jY5yBAYzV"
-
-    test_list = BtcAddressMonitoring(test_address).get_transactions_from_btc_address()
-
-    print(test_list)
-    print(len(test_list))
-
+    
 
 if __name__ == "__main__":
     main()
